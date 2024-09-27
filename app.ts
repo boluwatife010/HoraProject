2f357e818a1fafb93b7db6f5f2544002baf5e424
boluwatife010 <temitopeboluojo@gmail.com>
2024-09-27 13:02:58 +0100

    CHORE: worked on cors

--- app.ts
+++ app.ts
@@ -20,6 +20,7 @@ app.use(session({
     saveUninitialized: true,
     cookie: { secure: process.env.NODE_ENV === 'production' }
   }));
+  // added cors for frontend connection
   app.use(cors({
     origin: 'http://localhost:5173', 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
