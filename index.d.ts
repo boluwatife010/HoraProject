a16d6ac30566461bfa9972ccfb2d4470481b51ea
boluwatife010 <temitopeboluojo@gmail.com>
2024-10-03 16:34:29 +0100

    CHORE: worked on some errors

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,12 @@
-
+// index.d.ts
 import { Multer } from 'multer';
 
 declare global {
   namespace Express {
+    interface Multer {
+      File: Multer.File;
+    }
+
     interface Request {
       file?: Multer.File;
       files?: Multer.File[];
