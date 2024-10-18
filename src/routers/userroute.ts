16a788afa09f2374ed4621b17db882956f1d1d44
boluwatife010 <temitopeboluojo@gmail.com>
2024-10-18 13:33:24 +0100

    chore: fixed compilation error

--- src/routers/userroute.ts
+++ src/routers/userroute.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import passport from 'passport';
 import { authenticateToken } from '../auth/auth';
-import { userRegistrationHandler, userProfilePictureHandler,
+import { userRegistrationHandler, uploadProfilePictureHandler,
      userLoginHandler, updateUserHandler, resendUserOtpHandle,
      getAUserHandler,deleteAUserHandler, getAllUsersHandler, forgotPasswordHandler,
       resetPasswordHandler, changePasswordHandler, calculateProgressHandler,
@@ -20,6 +20,6 @@ router.get('/', getAllUsersHandler);
 router.get('/progress/:userId', calculateProgressHandler)
 router.delete('/delete/:id', authenticateToken,  deleteAUserHandler);
 router.put('/update-streak/:userid',authenticateToken, updateStreakHandler)
-router.post('/upload-picture/:userId',userProfilePictureHandler)
+router.post('/upload-picture/:userId',uploadProfilePictureHandler)
 router.post('/resend-otp/:id', resendUserOtpHandle)
 export default router;
