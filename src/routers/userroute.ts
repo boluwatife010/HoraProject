e7426bff921714c1973321eb7f1be8016f4ff358
boluwatife010 <temitopeboluojo@gmail.com>
2024-10-24 16:46:52 +0100

    chore: working on search by usernames

--- src/routers/userroute.ts
+++ src/routers/userroute.ts
@@ -5,7 +5,8 @@ import { userRegistrationHandler,
      userLoginHandler, updateUserHandler, resendUserOtpHandle,
      getAUserHandler,deleteAUserHandler, getAllUsersHandler, forgotPasswordHandler,
       resetPasswordHandler, changePasswordHandler, calculateProgressHandler,
-      verifyOtpHandler, verifyEmailOtpHandler,updateStreakHandler} from '../controllers/user.controller';
+      verifyOtpHandler, verifyEmailOtpHandler,updateStreakHandler,
+      searchUserByUsernameHandler} from '../controllers/user.controller';
 const router = express.Router()
 router.post('/register', userRegistrationHandler );
 router.post('/verify-email/:id',verifyEmailOtpHandler)
@@ -20,6 +21,7 @@ router.get('/', getAllUsersHandler);
 router.get('/progress/:userId', calculateProgressHandler)
 router.delete('/delete/:id', authenticateToken,  deleteAUserHandler);
 router.put('/update-streak/:userid',authenticateToken, updateStreakHandler)
+router.get('/search-username', searchUserByUsernameHandler)
 //router.post('/upload-picture/:userId',uploadProfilePictureHandler)
 router.post('/resend-otp/:id', resendUserOtpHandle)
 export default router;
